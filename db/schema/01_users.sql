2144843d6b86986c2430123b35c16cec808e9323
goingdust <87334449+goingdust@users.noreply.github.com>
2022-01-08 14:51:05 -0500

    Revert "feature/db_schema"

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
 CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
-  description TEXT,
+  description TEXT NOT NULL,
   vote_link VARCHAR(255) NOT NULL,
   result_link VARCHAR(255) NOT NULL,
   creator_id INTEGER REFERENCES users(id) DELETE ON CASCADE
@@ -21,7 +21,6 @@ CREATE TABLE polls (
 CREATE TABLE options (
   id SERIAL PRIMARY KEY NOT NULL,
   label VARCHAR(255) NOT NULL,
-  label_description TEXT,
   points INTEGER NOT NULL,
   poll_id INTEGER REFERENCES polls(id) DELETE ON CASCADE
 );
