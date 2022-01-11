57733ca5880c04f4a730f8cc480570c07e169097
goingdust <dustinluck@gmail.com>
2022-01-11 03:16:29 +0000

    Write event listeners for vote items to implement drag and drop functionality. Begin to write functionality of ranking each option when dropped and submitted

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+  $('div.vote-items').on('dragstart', function(event) {
+    event.originalEvent.dataTransfer.setData('text/plain', event.target.id);
 
+    event.currentTarget.style.backgroundColor = 'yellow';
+  });
+
+  $('div.rank-options').on('dragover', function(event) {
+    event.preventDefault();
+  });
+
+  $('div.rank-options').on('drop', function(event) {
+    const id = event.originalEvent.dataTransfer.getData('text');
+
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+
+    event.originalEvent.dataTransfer.clearData();
+  });
+
+  $('button#form-submit-button').on('mouseover', function(event) {
+    const $voteItems = $('div.vote-items');
+    const totalPoints = [];
+
+    for (let i = 0; i < $voteItems.length; i++) {
+      const points = $voteItems.length - i;
+      totalPoints.push(points);
+    }
+  });
 
 });
--- routes/polls.js
+++ routes/polls.js
@@ -31,31 +31,31 @@ module.exports = (db, mailgun) => {
 
   // });
 
-  router.get('/:id', (req, res) => {
-    db.query(`SELECT title, description
-              FROM polls
-              JOIN users ON creator_id = users.id
-              WHERE users.id = $1`,[req.params.id])
-    .then(data => {
-      // const templateVars= {}
-      // for (let i = 0; i < data.rows.length; i++){
-      //   templateVars[i] = {
-      //     title: data.rows[i].title,
-      //     description: data.rows[i].description
-      //   }
-      // }
-      const polls = data.rows;
-      const templateVars = { polls };
-      console.log(data.rows);
-      // console.log(templateVars);
-      return res.render('polls', templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-  });
+  // router.get('/', (req, res) => {
+  //   db.query(`SELECT title, description
+  //             FROM polls
+  //             JOIN users ON creator_id = users.id
+  //             WHERE users.id = $1`,[req.params.id])
+  //   .then(data => {
+  //     // const templateVars= {}
+  //     // for (let i = 0; i < data.rows.length; i++){
+  //     //   templateVars[i] = {
+  //     //     title: data.rows[i].title,
+  //     //     description: data.rows[i].description
+  //     //   }
+  //     // }
+  //     const polls = data.rows;
+  //     const templateVars = { polls };
+  //     console.log(data.rows);
+  //     // console.log(templateVars);
+  //     return res.render('polls', templateVars);
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+  // });
 
   // router.post('/:id', (req, res) => {
 
