86745098ffcb2114987233409b276fb781332d51
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2022-01-14 00:34:15 +0000

    update seeds for presentation, route /polls after submit fixed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds
-INSERT INTO users (email, name) VALUES ('user1@decisions.com','Lucy');
+INSERT INTO users (email, name) VALUES ('lucianavivianigallo@gmail.com','Lucy');
 INSERT INTO users (email, name) VALUES ('user2@decisions.com','Lily');
 INSERT INTO users (email, name) VALUES ('goingdust@gmail.com','Dusty');
--- db/seeds/02_polls.sql
+++ db/seeds/02_polls.sql
@@ -1,9 +1,9 @@
 -- Polls table seeds
 INSERT INTO polls (creator_id, title, description, vote_link, result_link)
-VALUES (1, 'MOVIES', '2022/12/02' , 'http://localhost:8080/polls/1', 'http://localhost:8080/polls/1/result');
+VALUES (1, 'Movies', '2022/12/02' , 'http://localhost:8080/polls/1', 'http://localhost:8080/polls/1/result');
 
 INSERT INTO polls (creator_id, title, description, vote_link, result_link)
-VALUES (2, 'Italian Restaurant', '2022/12/02', 'http://localhost:8080/polls/2', 'http://localhost:8080/polls/2/results');
+VALUES (1, 'Italian Restaurant', '2022/12/02', 'http://localhost:8080/polls/2', 'http://localhost:8080/polls/2/results');
 
 INSERT INTO polls (creator_id, title, description, vote_link, result_link)
 VALUES (2, 'Summer Vacation 2022', '2021/08/02' , 'http://localhost:8080/polls/3', 'http://localhost:8080/polls/3/results');
